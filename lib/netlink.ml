75190795b8c4c84595fc692f67e7fda0e20cf79c
Rob Hoes <rob.hoes@citrix.com>
2013-07-02 15:52:30 +0100

    renamed some functions and added link_get_stat

--- lib/netlink.ml
+++ lib/netlink.ml
@@ -9,7 +9,8 @@ let int_of_protocol = function
 	| NETLINK_ROUTE -> 0
 
 let protocol_of_int = function
-	| _ -> NETLINK_ROUTE
+	| 0 -> NETLINK_ROUTE
+	| _ -> invalid_arg "protocol"
 
 let protocol = view ~read:protocol_of_int ~write:int_of_protocol int
 
@@ -25,6 +26,23 @@ let close = foreign ~from:libnl "nl_close" (ptr socket @-> returning int)
 module Route = struct
 	type link
 
+	type link_stat_id = RX_PACKETS | TX_PACKETS | RX_BYTES | TX_BYTES
+
+	let int_of_link_stat_id = function
+		| RX_PACKETS -> 0
+		| TX_PACKETS -> 1
+		| RX_BYTES -> 2
+		| TX_BYTES -> 3
+
+	let link_stat_id_of_int = function
+		| 0 -> RX_PACKETS
+		| 1 -> TX_PACKETS
+		| 2 -> RX_BYTES
+		| 3 -> TX_BYTES
+		| _ -> invalid_arg "link_stat_id"
+
+	let link_stat_id = view ~read:link_stat_id_of_int ~write:int_of_link_stat_id int
+
 	let libnl_route = Dl.dlopen ~filename:"libnl-route-3.so" ~flags:[Dl.RTLD_NOW]
 
 	let cache = ptr void
@@ -38,5 +56,8 @@ module Route = struct
 
 	let link_get_mtu = foreign ~from:libnl_route "rtnl_link_get_mtu"
 		(ptr link @-> returning int)
+
+	let link_get_stat = foreign ~from:libnl_route "rtnl_link_get_stat"
+		(ptr link @-> link_stat_id @-> returning uint64_t)
 end
 
--- test/test.ml
+++ test/test.ml
@@ -1,61 +1,22 @@
 open Ctypes
-open PosixTypes
-open Foreign
-
-type socket
-type link
-
-type protocol = NETLINK_ROUTE
-
-let int_of_protocol = function
-	| NETLINK_ROUTE -> 0
-
-let protocol_of_int = function
-	| _ -> NETLINK_ROUTE
-
-let protocol = view ~read:protocol_of_int ~write:int_of_protocol int
 
 let _ =
-	(* open libnl *)
-
-	let libnl = Dl.dlopen ~filename:"libnl-3.so" ~flags:[Dl.RTLD_NOW] in
-	let libnl_route = Dl.dlopen ~filename:"libnl-route-3.so" ~flags:[Dl.RTLD_NOW] in
-
-	(* setup ctypes *)
-
-	let socket : socket structure typ = structure "nl_sock" in
-
-	let nl_socket_alloc = foreign ~from:libnl "nl_socket_alloc" (void @-> returning (ptr socket)) in
-	let nl_socket_free = foreign ~from:libnl "nl_socket_free" (ptr socket @-> returning void) in
-	let nl_connect = foreign ~from:libnl "nl_connect" (ptr socket @-> protocol @-> returning int) in
-	let nl_close = foreign ~from:libnl "nl_close" (ptr socket @-> returning int) in
-
-	let nl_cache = ptr void in
-	let link : link structure typ = structure "rtnl_link" in
-
-	let rtnl_link_alloc_cache = foreign ~from:libnl_route "rtnl_link_alloc_cache"
-		(ptr socket @-> int @-> ptr nl_cache @-> returning int) in
-
-	let rtnl_link_get_by_name = foreign ~from:libnl_route "rtnl_link_get_by_name"
-		(nl_cache @-> string @-> returning (ptr link)) in
-
-	let rtnl_link_get_mtu = foreign ~from:libnl_route "rtnl_link_get_mtu"
-		(ptr link @-> returning int) in
-
-	(* main *)
-
 	print_endline "started.";
-	let s = nl_socket_alloc () in
-	let _ = nl_connect s NETLINK_ROUTE in
+	let s = Netlink.socket_alloc () in
+	let _ = Netlink.connect s Netlink.NETLINK_ROUTE in
 
-	let cache = allocate nl_cache null in
-	let _ = rtnl_link_alloc_cache s 0 cache in
-	let link = rtnl_link_get_by_name (!@ cache) "eth0" in
-	let mtu = rtnl_link_get_mtu link in
+	let cache = allocate Netlink.Route.cache null in
+	let _ = Netlink.Route.link_alloc_cache s 0 cache in
+	let link = Netlink.Route.link_get_by_name (!@ cache) "eth0" in
+	let mtu = Netlink.Route.link_get_mtu link in
 	Printf.printf "MTU: %d\n" mtu;
 
-	let _ = nl_close s in
-	nl_socket_free s;
-	print_endline "done."
+	let tx_bytes = Netlink.Route.link_get_stat link Netlink.Route.TX_BYTES in
+	Printf.printf "TX bytes: %d\n" (Unsigned.UInt64.to_int tx_bytes);
+	let rx_bytes = Netlink.Route.link_get_stat link Netlink.Route.RX_BYTES in
+	Printf.printf "RX bytes: %d\n" (Unsigned.UInt64.to_int rx_bytes);
 
+	let _ = Netlink.close s in
+	Netlink.socket_free s;
+	print_endline "done."
 
